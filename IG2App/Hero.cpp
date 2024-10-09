1ce191099fb27d11ddfbce1163116c8907491446
Andrés García Navarro <andrga37@ucm.es>
2024-10-09 11:37:11 +0200

    Soy Pablo: ya se mueve bien

--- IG2App/Hero.cpp
+++ IG2App/Hero.cpp
@@ -11,42 +11,39 @@ Hero::~Hero()
 
 void Hero::frameRendered(const Ogre::FrameEvent& evt)
 {
-	mNode->translate(direction);
+	mNode->translate(Vector3{ 0, 0, 1 }, Ogre::Node::TransformSpace::TS_LOCAL);
 }
 
 bool Hero::keyPressed(const OgreBites::KeyboardEvent& evt)
 {
-
 	switch (evt.keysym.sym)
 	{
 	case SDLK_UP:
-		direction = { 0, 0,1 };
+		direction = { 0, 0, 1 };
 
 		break;
 	case SDLK_DOWN:
-		direction = { 0, 0, -1};
+		direction = { 0, 0, -1 };
 
 		break;
 	case SDLK_LEFT:
 
-		direction = { 1, 0,0 };
+		direction = { 1, 0, 0 };
 
 		break;
 	case SDLK_RIGHT:
-		
-		direction = { -1, 0,0 };
+
+		direction = { -1, 0, 0 };
 		break;
 	default:
 		break;
 	}
-
+	rotate();
 	return false;
 }
 
 void Hero::rotate()
 {
-	if (direction != Vector3{0, 0, 0})
-	{
-
-	}
+	Quaternion q = this->getOrientation().getRotationTo(direction);
+	mNode->rotate(q);
 }
