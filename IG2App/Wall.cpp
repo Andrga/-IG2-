af50e26b65d1c3d0cc12b76c44e989c75c7c41e0
Andrés García Navarro <andrga37@ucm.es>
2024-10-15 12:40:06 +0200

    amai la kuki

--- IG2App/Wall.cpp
+++
@@ -1,7 +0,0 @@
-#include "Wall.h"
-
-Wall::Wall(Vector3 initPos, SceneNode* node, SceneManager* sceneMng, String mesh, string name)
-	: Block(initPos, node->createChildSceneNode(name), sceneMng, mesh)
-{
-	blockType = BLOCK_TYPE::PEARL;
-}
