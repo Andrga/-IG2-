e74b4df41b1c01ab137fc6893fd70cbabd5afe39
Paigro <paigro9@gmail.com>
2024-11-19 12:38:07 +0100

    Esfera naranja.

--- IG2App/IG2App.h
+++ IG2App/IG2App.h
@@ -20,32 +20,34 @@
 #include <iostream>
 #include <string>
 
-class IG2App : public  OgreBites::IG2ApplicationContext, OgreBites::InputListener{
+class IG2App : public  OgreBites::IG2ApplicationContext, OgreBites::InputListener {
 
 public:
-    explicit IG2App() : IG2ApplicationContext("IG2App") { };
-    virtual ~IG2App() { };
- 
+	explicit IG2App() : IG2ApplicationContext("IG2App") { };
+	virtual ~IG2App() { };
+
 protected:
-    virtual bool keyPressed(const OgreBites::KeyboardEvent& evt);
-    virtual void setup();
-    virtual void shutdown();
-    virtual void setupScene();
-    virtual void frameRendered(const Ogre::FrameEvent& evt);
-    
+	virtual bool keyPressed(const OgreBites::KeyboardEvent& evt);
+	virtual void setup();
+	virtual void shutdown();
+	virtual void setupScene();
+	virtual void frameRendered(const Ogre::FrameEvent& evt);
+	bool frameEnded(const Ogre::FrameEvent& evt)override;
+
+
 private:
-    
-    // Sinbad
-    Ogre::SceneNode* sinbadNode = nullptr;
-    Ogre::Entity* sinbadEnt;
-    
-    // Scene manager and tray system
-    Ogre::SceneManager* mSM = nullptr;
-    OgreBites::TrayManager* mTrayMgr = nullptr;
-   
-    // Camera
-    Ogre::SceneNode* mCamNode = nullptr;
-    OgreBites::CameraMan* mCamMgr = nullptr;
+
+	// Sinbad
+	Ogre::SceneNode* sinbadNode = nullptr;
+	Ogre::Entity* sinbadEnt;
+
+	// Scene manager and tray system
+	Ogre::SceneManager* mSM = nullptr;
+	OgreBites::TrayManager* mTrayMgr = nullptr;
+
+	// Camera
+	Ogre::SceneNode* mCamNode = nullptr;
+	OgreBites::CameraMan* mCamMgr = nullptr;
 };
 
 #endif
